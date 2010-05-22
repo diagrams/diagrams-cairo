d4fbf74ff013ee26911ac1eadc55d0b951b89cbc
Brent Yorgey <byorgey@cis.upenn.edu>
2010-05-22 01:41:58 +0000

    add simple example

---
+++ example/boxes.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE PackageImports #-}
+
+import "diagrams-core" Graphics.Rendering.Diagrams
+import Graphics.Rendering.Diagrams.Transform
+
+import Diagrams.TwoD
+
+import Diagrams.Backend.Cairo
+
+d :: Diagram Cairo
+d = translate (30,30) $ scale 10 (beside (2,1) box box `atop` box)
+
+main = renderDia Cairo [] d
