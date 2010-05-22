902129ce2f0c172471d68b4b977ff5b1c371da7a
Brent Yorgey <byorgey@cis.upenn.edu>
2010-05-22 01:32:44 +0000

    fix Cairo backend to work with new formulation of Backend class

--- src/Diagrams/Backend/Cairo.hs
+++ src/Diagrams/Backend/Cairo.hs
@@ -3,8 +3,7 @@ module Diagrams.Backend.Cairo where
 
 import qualified Graphics.Rendering.Cairo as C
 
-import Graphics.Rendering.Diagrams.Backends
-import Graphics.Rendering.Diagrams.Renderable
+import Graphics.Rendering.Diagrams
 
 import Diagrams.TwoD
 
@@ -12,9 +11,12 @@ data Cairo = Cairo
 
 instance Backend Cairo where
   type BSpace Cairo = P2
-  type Render Cairo = C.Render
-  runRender _ _ r = C.withPDFSurface "test.pdf" 100 100 $ \surface ->
-                    C.renderWith surface r
+  type Render Cairo = C.Render ()
+  type Result Cairo = IO ()
+  data Option Cairo = OutputFile String  -- XXX add more options!
+
+  renderDia _ _ d = C.withPDFSurface "test.pdf" 100 100 $ \surface ->
+                    C.renderWith surface (mapM_ (render Cairo) (prims d))
 
 instance Renderable Box Cairo where
   render _ (Box v1 v2 v3 v4) = do
