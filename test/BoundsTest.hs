02823effd43c6bd9983d5d6572d0fbde631c301a
Brent Yorgey <byorgey@cis.upenn.edu>
2010-11-08 22:00:23 +0000

    add some tests

---
+++ test/BoundsTest.hs
@@ -0,0 +1,45 @@
+import Graphics.Rendering.Diagrams
+import Graphics.Rendering.Diagrams.Transform
+
+import Diagrams.TwoD
+
+import Diagrams.Backend.Cairo
+import Diagrams.Combinators
+import Diagrams.Path
+import Diagrams.Segment
+
+import Data.VectorSpace
+
+p = stroke $ Path False zeroV [Linear (1.0,0.0),Linear (0.0,1.0)]
+bez = stroke $ Path False zeroV [Cubic (1.0,0.0) (0.0,1.0) (1.0,1.0)]
+ell = horizontalScale 2 $ verticalScale 0.5 circle
+
+-- correct:
+b1 = runBoundsTest box
+b2 = runBoundsTest circle
+b3 = runBoundsTest p
+
+-- failures:
+b4 = runBoundsTest bez
+b5 = runBoundsTest ell
+b6 = runBoundsTest (scale 2 box)
+b7 = runBoundsTest (scale 2 circle)
+b8 = runBoundsTest (scale 2 p)
+b9 = runBoundsTest (scale 2 bez)
+b10 = runBoundsTest (scale 2 ell)
+
+runBoundsTest :: Diagram Cairo -> Diagram Cairo
+runBoundsTest d = scale 20 $ translate (10,10) (sampleBounds2D 10 d)
+
+sampleBounds2D :: Int -> Diagram Cairo -> Diagram Cairo
+sampleBounds2D n d = foldr atop d bs
+    where b  = getBounds (bounds d)
+          bs = [stroke $ mkLine (s *^ v) (perp v) | v <- vs, let s = b v]
+          vs = [(cos t, sin t) | i <- [0..n]
+                               , let t = ((fromIntegral i) * 2.0 * pi) / (fromIntegral n)]
+          mkLine a v = Path False a [Linear v]
+          perp (x,y) = (-y,x)
+          getBounds (Bounds f) = f
+
+opts = CairoOptions "test2.pdf" $ PDF (400, 400)
+main = renderDia Cairo opts b5
---
+++ test/EllipseBounds.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE PackageImports #-}
+
+import "diagrams-core" Graphics.Rendering.Diagrams
+import Graphics.Rendering.Diagrams.Transform
+
+import Diagrams.TwoD
+
+import Diagrams.Backend.Cairo
+import Diagrams.Combinators
+
+d :: Diagram Cairo
+d = translate (30,30) $ scale 10 (beside (2,1) box box `atop` circle)
+
+d' :: Diagram Cairo
+d' = translate (30,30) $ scale 10 (beside (2,1) box box `atop` ell1)
+
+ell1 = rotate (-pi/6) $ horizontalScale 2 $ verticalScale 0.5 circle
+
+d2, d2', d3, d4, d5 :: Diagram Cairo
+d2 = translate (30,30) $ scale 10 (beside (2,1) box ell1)  -- intersects
+d2' = translate (30,30) $ scale 10 (beside (4,1) box ell1)  -- intersects
+
+d3 = translate (30,30) $ scale 10 (beside (2,1) box circle)
+d4 = translate (30,30) $ scale 10 (beside (50,50) box ell1)
+d5 = translate (30,30) $ scale 10 (beside (2,2) box circle)
+
+e :: Diagram Cairo
+e = translate (50,70) $ rotate (pi/6) $ horizontalScale 30 $ verticalScale 10 circle
+
+ell = rotate (pi/6) $ horizontalScale 30 $ verticalScale 10 circle
+box10 = scale 10 box
+
+f :: Diagram Cairo
+f = translate (50,70) $ beside (1,0) box10 ell
+
+opts = CairoOptions "test3.pdf" $ PDF (400, 400)
+main = renderDia Cairo opts d2'
