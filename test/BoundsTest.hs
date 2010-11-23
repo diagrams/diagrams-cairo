397b6f5db92e5e23dab6010e6fabcddf74902c4c
Brent Yorgey <byorgey@cis.upenn.edu>
2010-11-23 21:28:54 +0000

    fix up BoundsTest to work with changes to core

--- test/BoundsTest.hs
+++ test/BoundsTest.hs
@@ -10,8 +10,8 @@ import Diagrams.Segment
 
 import Data.VectorSpace
 
-p = stroke $ Path False zeroV [Linear (1.0,0.0),Linear (0.0,1.0)]
-bez = stroke $ Path False zeroV [Cubic (1.0,0.0) (0.0,1.0) (1.0,1.0)]
+p = stroke $ Path False origin [Linear (1.0,0.0),Linear (0.0,1.0)]
+bez = stroke $ Path False origin [Cubic (1.0,0.0) (0.0,1.0) (1.0,1.0)]
 ell = scaleX 2 $ scaleY 0.5 circle
 
 b1 = runBoundsTest box
@@ -34,7 +34,7 @@ runBoundsTest d = scale 20 $ translate (10,10) (sampleBounds2D 10 d)
 sampleBounds2D :: Int -> Diagram Cairo -> Diagram Cairo
 sampleBounds2D n d = foldr atop d bs
     where b  = getBounds (bounds d)
-          bs = [stroke $ mkLine (s *^ v) (perp v) | v <- vs, let s = b v]
+          bs = [stroke $ mkLine (P $ s *^ v) (perp v) | v <- vs, let s = b v]
           vs = [(cos t, sin t) | i <- [0..n]
                                , let t = ((fromIntegral i) * 2.0 * pi) / (fromIntegral n)]
           mkLine a v = Path False a [Linear v]
@@ -42,4 +42,4 @@ sampleBounds2D n d = foldr atop d bs
           getBounds (Bounds f) = f
 
 opts = CairoOptions "test2.pdf" $ PDF (400, 400)
-main = renderDia Cairo opts b13
+main = renderDia Cairo opts b3
