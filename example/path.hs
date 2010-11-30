1e1eaa7d641c825bffbb75f8bf966a1e5501e2e7
Brent Yorgey <byorgey@cis.upenn.edu>
2010-11-30 19:58:55 +0000

    fix path example so it compiles again

--- example/path.hs
+++ example/path.hs
@@ -10,6 +10,8 @@ import Diagrams.Path
 import Diagrams.Backend.Cairo
 
 d :: Diagram Cairo
-d = scale 10 (path [(1,1), (1,2), (1,3), (7,2)])
+d = stroke $ pathFromOffsets (P (0,0)) [(1,1), (1,2), (1,3), (7,2)]
 
-main = renderDia Cairo [] d
+opts = CairoOptions "path.pdf" (PDF (100, 100))
+
+main = renderDia Cairo opts d
