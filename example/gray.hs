f018f69275883ed234ff1a1e82a0ab483de23de5
Brent Yorgey <byorgey@cis.upenn.edu>
2010-12-04 10:16:22 +0000

    add gray.hs example

---
+++ example/gray.hs
@@ -0,0 +1,31 @@
+import Graphics.Rendering.Diagrams
+import Diagrams.TwoD.Arc
+import Diagrams.TwoD
+import Diagrams.Path
+import Diagrams.Backend.Cairo
+import Data.List.Split
+import Data.Maybe
+import Control.Applicative
+import Data.Monoid
+import Data.List
+
+gray 0 = [[]]
+gray n = map (False:) g ++ map (True:) (reverse g)
+  where g = gray (n-1)
+
+rings n = mkRingsDia . map ringOffsets . transpose . gray $ n
+  where ringOffsets :: [Bool] -> [(Angle, Angle)]
+        ringOffsets = map l2t . chunk 2 . findEdges . zip [0,2*pi/(2^n)..2*pi]
+        l2t [x,y] = (x,y)
+        l2t [x]   = (x,2*pi)
+
+findEdges :: Eq a => [(Angle, a)] -> [Angle]
+findEdges = catMaybes . (zipWith edge <*> tail)
+  where edge (_,c1) (a,c2) | c1 /= c2  = Just a
+                           | otherwise = Nothing
+
+mkRingsDia :: [[(Angle, Angle)]] -> Diagram Cairo
+mkRingsDia = mconcat . zipWith mkRingDia [1,1.5..]
+  where mkRingDia r = mconcat . map (stroke . scale r . uncurry arc)
+
+main = renderDia Cairo (CairoOptions "gray.pdf" $ PDF (400, 400)) (rings 10)
