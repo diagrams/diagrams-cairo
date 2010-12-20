783fa10530fdcedf84d27bcd142350ba78313b10
ksuvee <ksuvee@seas.upenn.edu>
2010-12-20 20:28:30 +0000

    square_with_polygon

--- example/square.hs
+++ example/square.hs
@@ -4,6 +4,6 @@ import Diagrams.TwoD.Shapes
 import Data.Colour.SRGB
 
 s :: Diagram Cairo
-s = fc (sRGB 0.4 0.4 0.4) square
+s = fc (sRGB 0.4 0.4 0.4) $ lw 0 $ rotate (pi/4) (polygon 4)
 
 main = renderDia Cairo (CairoOptions "Square.pdf" $ PDF (400, 400)) s
