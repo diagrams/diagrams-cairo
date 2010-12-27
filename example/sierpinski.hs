6c378bee5322aa2a37c44f59cd2445fb96165320
Brent Yorgey <byorgey@cis.upenn.edu>
2010-12-27 00:03:54 +0000

    fix sierpinski example

--- example/sierpinski.hs
+++ example/sierpinski.hs
@@ -4,7 +4,7 @@ import Diagrams.TwoD.Shapes
 import Data.Colour.SRGB
 
 sierpinski :: Int -> Diagram Cairo
-sierpinski 0 = fc (sRGB 0.4 0.4 0.4) $ lw 0 $ (rotate (pi/2) (polygon 3))
+sierpinski 0 = fc (sRGB 0.4 0.4 0.4) $ lw 0 $ (rotate (pi/2) (polygon with {sides=3}))
 sierpinski n = beside (0, 1) (beside (1, 0) s s) s
   where s = sierpinski (n-1)
 
