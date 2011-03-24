49d440ffc98354061e9f20f1acd32fe302475276
Brent Yorgey <byorgey@cis.upenn.edu>
2011-03-24 20:26:43 +0000

    update beside to use new CmdLine module

--- example/beside.hs
+++ example/beside.hs
@@ -1,10 +1,19 @@
+{-# LANGUAGE TypeFamilies, FlexibleContexts #-}
 import Diagrams.Prelude
 
 import Diagrams.Backend.Cairo
+import Diagrams.Backend.Cairo.CmdLine
 
-b :: Diagram Cairo
+import Diagrams.TwoD.Ellipse
+
+b :: (BSpace b ~ R2, Renderable Ellipse b, Renderable (Path R2) b) => Diagram b
 b = beside (1,1) (rotate (pi/4) $ scaleY 2 circle) square
 
+e1 :: (BSpace b ~ R2, Renderable Ellipse b, Renderable (Path R2) b) => Diagram b
+e1 = rotate (pi/4) $ scaleY 2 circle
+
+e2 :: (BSpace b ~ R2, Renderable Ellipse b, Renderable (Path R2) b) => Diagram b
+e2 = translateX 1 $ e1
+
 main :: IO ()
-main = renderDia Cairo opts b
-  where opts = CairoOptions "beside.pdf" $ PDF (400,400)
+main = defaultMain b
