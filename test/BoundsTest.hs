c1efb97fcb59889a16dd488b23e8fbcd8efa8f50
Brent Yorgey <byorgey@cis.upenn.edu>
2011-03-30 03:24:41 +0000

    get BoundsTest compiling again

--- test/BoundsTest.hs
+++ test/BoundsTest.hs
@@ -10,7 +10,8 @@ import Diagrams.Segment
 
 import Data.VectorSpace
 
-p, bez, ell :: Diagram Cairo
+type D = Diagram Cairo R2
+
 p = stroke $ fromSegments [Linear (1.0,0.0),Linear (0.0,1.0)]
 bez = stroke $ fromSegments [Cubic (1.0,0.0) (0.0,1.0) (1.0,1.0)]
 ell = scaleX 2 $ scaleY 0.5 circle
@@ -30,13 +31,12 @@ b12 = runBoundsTest (scaleX 3 $ scaleY 2 $ bez)
 b13 = runBoundsTest (translate (1,0) square)
 b14 = runBoundsTest (rotate (2*pi/3) ell)
 
-runBoundsTest :: Diagram Cairo -> Diagram Cairo
+runBoundsTest :: D -> D
 runBoundsTest = sampleBounds2D 10
 
-sampleBounds2D :: Int -> Diagram Cairo -> Diagram Cairo
+sampleBounds2D :: Int -> D -> D
 sampleBounds2D n d = foldr atop d bs
     where b  = getBounds (bounds d)
-          bs :: [Diagram Cairo]
           bs = [stroke $ mkLine (P $ s *^ v) (perp v) | v <- vs, let s = b v]
           vs = [(2 * cos t, 2 * sin t) | i <- [0..n]
                                        , let t = ((fromIntegral i) * 2.0 * pi) / (fromIntegral n)]
@@ -44,4 +44,4 @@ sampleBounds2D n d = foldr atop d bs
           perp (x,y) = (-y,x)
           getBounds (Bounds f) = f
 
-main = defaultMain b11
+main = defaultMain b12
