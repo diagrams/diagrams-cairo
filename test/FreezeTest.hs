9f0af5416470d4676b0b2ad75f5adeeae5a279ea
Brent Yorgey <byorgey@cis.upenn.edu>
2011-04-02 15:30:33 +0000

    import FreezeTest.hs.  Trying to get to the bottom of the funny stuff going on with freeze.

---
+++ test/FreezeTest.hs
@@ -0,0 +1,29 @@
+import Diagrams.Prelude
+
+import Diagrams.Backend.Cairo.CmdLine
+
+type D = Diagram Cairo R2
+
+s :: D
+s = square
+
+s' = square # lw 0.03
+            # lc black
+
+s'' = s' # freeze
+
+d :: D
+d = hcat' ((with :: CatOpts R2) { sep = 2 })
+--  [ s' , s'  # scaleX 5, s'  # scaleY 5, s'  # scale 5
+  [ s'', s'' # scaleX 5, s'' # scaleY 5, s'' # scale 5 ]
+--  , s' , s'  # scaleX 5 # freeze, s' # scaleY 5 # freeze, s' # scale 5 # freeze]
+
+c :: D
+c = circle # scale 20  -- # lc black  -- this changes the line width!!!
+
+cs = hcat' ((with :: CatOpts R2) { sep = 10 })
+     [ c, c1, c1 # scale 2, c1 # freeze # scale 2
+     , c1 # scaleY 3, c1 # freeze # scaleY 3 ]
+  where c1 = c # lw 1
+
+main = defaultMain (cs # centerXY # pad 1.1)
