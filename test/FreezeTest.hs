a91704e4e59c1e63fa0eceb0b11d535bd1e3f06c
Brent Yorgey <byorgey@cis.upenn.edu>
2011-04-03 01:38:51 +0000

    Proxy field means we no longer need type annotation on argument to hcat'

--- test/FreezeTest.hs
+++ test/FreezeTest.hs
@@ -13,7 +13,7 @@ s' = square # lw 0.03
 s'' = s' # freeze
 
 d :: D
-d = hcat' ((with :: CatOpts R2) { sep = 2 })
+d = hcat' with { sep = 2 }
 --  [ s' , s'  # scaleX 5, s'  # scaleY 5, s'  # scale 5
   [ s'', s'' # scaleX 5, s'' # scaleY 5, s'' # scale 5 ]
 --  , s' , s'  # scaleX 5 # freeze, s' # scaleY 5 # freeze, s' # scale 5 # freeze]
@@ -21,7 +21,7 @@ d = hcat' ((with :: CatOpts R2) { sep = 2 })
 c :: D
 c = circle # scale 20  -- # lc black  -- this changes the line width!!!
 
-cs = hcat' ((with :: CatOpts R2) { sep = 10 })
+cs = hcat' with { sep = 10 }
      [ c, c1, c1 # scale 2, c1 # freeze # scale 2
      , c1 # scaleY 3, c1 # freeze # scaleY 3 ]
   where c1 = c # lw 1
