fe7862f46ffbedffb6353384118ac52863f4e1db
Brent Yorgey <byorgey@cis.upenn.edu>
2011-04-03 01:38:00 +0000

    switch to V type family instead of MPTCs

--- src/Diagrams/Backend/Cairo.hs
+++ src/Diagrams/Backend/Cairo.hs
@@ -157,7 +157,7 @@ fromLineJoin LineJoinMiter = C.LineJoinMiter
 fromLineJoin LineJoinRound = C.LineJoinRound
 fromLineJoin LineJoinBevel = C.LineJoinBevel
 
-instance Renderable Ellipse Cairo R2 where
+instance Renderable Ellipse Cairo where
   render _ ell = do
     let P (xc,yc) = ellipseCenter ell
         (xs,ys)   = ellipseScale ell
@@ -171,16 +171,16 @@ instance Renderable Ellipse Cairo R2 where
     C.closePath
     C.restore
 
-instance Renderable (Segment R2) Cairo R2 where
+instance Renderable (Segment R2) Cairo where
   render _ (Linear v) = uncurry C.relLineTo v
   render _ (Cubic (x1,y1) (x2,y2) (x3,y3)) = C.relCurveTo x1 y1 x2 y2 x3 y3
 
-instance Renderable (Trail R2) Cairo R2 where
+instance Renderable (Trail R2) Cairo where
   render _ (Trail segs c) = do
     mapM_ (render Cairo) segs
     when c $ C.closePath
 
-instance Renderable (Path R2) Cairo R2 where
+instance Renderable (Path R2) Cairo where
   render _ (Path trs) = C.newPath >> F.mapM_ renderTrail trs
     where renderTrail (tr, P p) = do
             uncurry C.moveTo p
