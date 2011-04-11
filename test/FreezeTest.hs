c36178548225ac886ce0142244f553c168f52bb0
Brent Yorgey <byorgey@cis.upenn.edu>
2011-04-11 00:42:48 +0000

    FreezeTest works now

--- test/FreezeTest.hs
+++ test/FreezeTest.hs
@@ -14,16 +14,16 @@ s'' = s' # freeze
 
 d :: D
 d = hcat' with { sep = 2 }
---  [ s' , s'  # scaleX 5, s'  # scaleY 5, s'  # scale 5
-  [ s'', s'' # scaleX 5, s'' # scaleY 5, s'' # scale 5 ]
---  , s' , s'  # scaleX 5 # freeze, s' # scaleY 5 # freeze, s' # scale 5 # freeze]
+  [ s' , s'  # scaleX 5, s'  # scaleY 5, s'  # scale 5
+  , s'', s'' # scaleX 5, s'' # scaleY 5, s'' # scale 5 ]
 
 c :: D
-c = circle # scale 20  -- # lc black  -- this changes the line width!!!
+c = circle # scale 20 # lc red
 
 cs = hcat' with { sep = 10 }
      [ c, c1, c1 # scale 2, c1 # freeze # scale 2
-     , c1 # scaleY 3, c1 # freeze # scaleY 3 ]
+     , c1 # scaleY 3, c1 # freeze # scaleY 3, c1 # scaleY 3 # freeze ]
   where c1 = c # lw 1
 
-main = defaultMain (cs # centerXY # pad 1.1)
+main = defaultMain (d # centerXY # pad 1.1)
+       -- (cs # centerXY # pad 1.1)
