031de4a70e7688db8f18cbec27a8e24095ad49c5
Brent Yorgey <byorgey@cis.upenn.edu>
2011-04-18 21:20:55 +0000

    remove star.hs

--- example/star.hs
+++
@@ -1,7 +0,0 @@
-import Diagrams.Prelude
-import Diagrams.Backend.Cairo
-import Data.Colour.Names
-
-d = fc blue $ starPolygon 17 5
-
-main = renderDia Cairo (CairoOptions "Star.pdf" (PDF (100.0,100.0))) d
