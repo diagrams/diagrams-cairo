38d8769aa75957ea8e11096d67d64e238466c5ab
Brent Yorgey <byorgey@cis.upenn.edu>
2011-04-19 11:41:22 +0000

    testing bezier parameters -- looks OK.

---
+++ test/BezParam.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+import Diagrams.Prelude
+
+import Diagrams.Backend.Cairo.CmdLine
+
+type D = Diagram Cairo R2
+
+t :: Trail R2
+t = (polygonPath with {sides = 3, orientation = OrientToX})
+[v1,v2] = trailOffsets t
+
+seg = Cubic v1 v1 (v1 ^+^ v2)
+
+s :: Trail R2
+s = fromSegments [seg]
+
+pts = map ((origin .+^) . (seg `atParam`)) [0,0.01..1]
+showPts = position (pts `zip` repeat dot)
+  where dot = circle # lw 0 # fc red # scale 0.01
+
+-- s1 = (strokeT $ s <> (rotateBy (1/3) s) <> (rotateBy (2/3) s))
+--    # centerXY
+
+dia :: D
+dia = pad 1.1 (showPts `atop` strokeT s)
+
+main = defaultMain dia
