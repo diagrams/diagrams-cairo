0438e9fd6c34a81ecc13f3f0785257e12401b254
Brent Yorgey <byorgey@cis.upenn.edu>
2011-05-04 19:49:44 +0000

    more inside/outside testing, querying stroked paths now works

--- test/Inside2.hs
+++ test/Inside2.hs
@@ -4,32 +4,32 @@ import Diagrams.Backend.Cairo.CmdLine
 
 import qualified Data.Set as S
 
+path = polygonPath with {sides = 8}
+
 -- t = fromOffsets [(1,5), (1,-5)]
 -- path = close . pathFromTrail . mconcat . take 8 . iterate (rotateBy (-1/8)) $ t
-
-       -- polygonPath with {sides=8}
 
 -- path = close . fromOffsets $ [(0,10), (3,0), (0, -8), (2,0), (0,2), (-4,0), (0, 3)
 --                              ,(9, 0), (0, -7)]
 
-t = (polygonPath with {sides = 3, orientation = OrientToX})
-[v1,v2] = trailOffsets t
+-- t = (polygonPath with {sides = 3, orientation = OrientToX})
+-- [v1,v2] = trailOffsets t
 
-seg = Cubic v1 v1 (v1 ^+^ v2)
+-- seg = Cubic v1 v1 (v1 ^+^ v2)
 
-path = centerXY . scale 5 . close $ fromSegments [seg]
+-- path = centerXY . scale 5 . close $ fromSegments [seg]
 
 dot c = circle # fc c
                # lw 0
-               # scale 0.03
+               # scale 0.01
 
-points = mconcat [ if (isInsideWinding p path) then dot red # moveTo p else mempty
+points = mconcat [ if getAny (sample oct p) then dot red # moveTo p else mempty
                  | x <- range
                  , y <- range
                  , let p = P (x,y)
                  ]
-  where range = [-5, -4.9 .. 5]
+  where range = [-1, -0.96 .. 1]
 
-main = do
-  print path
-  defaultMain (points <> stroke path # lw 0.05)
+oct = stroke path # lw 0.02
+
+main = defaultMain (points <> oct)
