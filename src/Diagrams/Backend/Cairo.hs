1afe1b84d74d329727d8296688b2328d20ddd272
Brent Yorgey <byorgey@cis.upenn.edu>
2011-06-07 01:40:15 +0000

    implement text slant, weight, and alignment

--- src/Diagrams/Backend/Cairo.hs
+++ src/Diagrams/Backend/Cairo.hs
@@ -30,15 +30,15 @@ import Graphics.Rendering.Diagrams.Transform
 import Diagrams.Prelude
 import Diagrams.TwoD.Ellipse
 import Diagrams.TwoD.Path (Clip(..))
-import Diagrams.TwoD.Text (Text(..), getFontSize)
+import Diagrams.TwoD.Text
 import Diagrams.TwoD.Adjust (adjustDia2D)
 
 import qualified Graphics.Rendering.Cairo as C
 import qualified Graphics.Rendering.Cairo.Matrix as CM
 
 import Control.Applicative ((<$>))
-import Control.Monad (when)
-import Data.Maybe (catMaybes)
+import Control.Monad.State
+import Data.Maybe (catMaybes, fromMaybe)
 
 import Data.Monoid
 import qualified Data.Foldable as F
@@ -67,8 +67,22 @@ instance Monoid (Render Cairo R2) where
   mempty  = C $ return ()
   (C r1) `mappend` (C r2) = C (r1 >> r2)
 
+type RenderM a = StateT TextAlignment C.Render a
+
+-- simple, stupid implementations of save and restore for now, since
+-- it suffices to just reset the text alignment to "centered" on
+-- restore.  But if need be we can switch to a more sophisticated
+-- implementation using an "undoable state" monad which lets you save
+-- (push state onto a stack) and restore (pop from the stack).
+
+save :: RenderM ()
+save = lift C.save
+
+restore :: RenderM ()
+restore = put defaultTextAlignment >> lift C.restore
+
 instance Backend Cairo R2 where
-  data Render  Cairo R2 = C (C.Render ())
+  data Render  Cairo R2 = C (RenderM ())
   type Result  Cairo R2 = (IO (), C.Render ())
   data Options Cairo R2 = CairoOptions
           { fileName     :: String       -- ^ the name of the file you want generated
@@ -76,17 +90,19 @@ instance Backend Cairo R2 where
           }
 
   withStyle _ s t (C r) = C $ do
-    C.save
+    save
     cairoMiscStyle s
     r
-    cairoTransf t
-    cairoStrokeStyle s
-    C.stroke
-    C.restore
+    lift $ do
+      cairoTransf t
+      cairoStrokeStyle s
+      C.stroke
+    restore
 
-  doRender _ options (C r) = (renderIO, r)
-    where renderIO = do
-            let surfaceF s = C.renderWith s r
+  doRender _ options (C r) = (renderIO, r')
+    where r' = evalStateT r defaultTextAlignment
+          renderIO = do
+            let surfaceF s = C.renderWith s r'
                 file = fileName options
             case outputFormat options of
               PNG (w,h) ->
@@ -103,19 +119,39 @@ instance Backend Cairo R2 where
           getSize (PDF sz) = sz
           getSize (SVG sz) = sz
 
-renderC :: (Renderable a Cairo, V a ~ R2) => a -> C.Render ()
+renderC :: (Renderable a Cairo, V a ~ R2) => a -> RenderM ()
 renderC a = case (render Cairo a) of C r -> r
 
-cairoMiscStyle :: Style v -> C.Render ()
+cairoMiscStyle :: Style v -> RenderM ()
 cairoMiscStyle s =
   sequence_
   . catMaybes $ [ handle clip
                 , handle fSize
+                , handleFontFace
+                , handle tAlign
+                , handle fColor
                 ]
-  where handle :: (AttributeClass a) => (a -> C.Render ()) -> Maybe (C.Render ())
+  where handle :: AttributeClass a => (a -> RenderM ()) -> Maybe (RenderM ())
         handle f = f `fmap` getAttr s
-        clip  = mapM_ (\p -> renderC p >> C.clip) . getClip
-        fSize = C.setFontSize . getFontSize
+        clip     = mapM_ (\p -> renderC p >> lift C.clip) . getClip
+        fSize    = lift . C.setFontSize . getFontSize
+        fFace    = fromMaybe "" $ getFont <$> getAttr s
+        fSlant   = fromFontSlant  . fromMaybe FontSlantNormal
+                 $ getFontSlant  <$> getAttr s
+        fWeight  = fromFontWeight . fromMaybe FontWeightNormal
+                 $ getFontWeight <$> getAttr s
+        handleFontFace = Just . lift $ C.selectFontFace fFace fSlant fWeight
+        tAlign   = put  -- remember the text alignment for later
+        fColor c = lift $ setSource (getFillColor c) s
+
+fromFontSlant :: FontSlant -> C.FontSlant
+fromFontSlant FontSlantNormal   = C.FontSlantNormal
+fromFontSlant FontSlantItalic   = C.FontSlantItalic
+fromFontSlant FontSlantOblique  = C.FontSlantOblique
+
+fromFontWeight :: FontWeight -> C.FontWeight
+fromFontWeight FontWeightNormal = C.FontWeightNormal
+fromFontWeight FontWeightBold   = C.FontWeightBold
 
 cairoStrokeStyle :: Style v -> C.Render ()
 cairoStrokeStyle s =
@@ -129,24 +165,21 @@ cairoStrokeStyle s =
                 ]
   where handle :: (AttributeClass a) => (a -> C.Render ()) -> Maybe (C.Render ())
         handle f = f `fmap` getAttr s
-        fColor c = do
-          let (r,g,b,a) = colorToRGBA . getFillColor $ c
-          let a' = case getOpacity <$> getAttr s of
-                     Nothing -> a
-                     Just d  -> a * d
-          C.setSourceRGBA r g b a'
-          C.fillPreserve
-        lColor c = do
-          let (r,g,b,a) = colorToRGBA . getLineColor $ c
-          let a' = case getOpacity <$> getAttr s of
-                     Nothing -> a
-                     Just d  -> a * d
-          C.setSourceRGBA r g b a'
+        fColor c = setSource (getFillColor c) s >> C.fillPreserve
+        lColor c = setSource (getLineColor c) s
         lWidth = C.setLineWidth . getLineWidth
         lCap   = C.setLineCap . fromLineCap . getLineCap
         lJoin  = C.setLineJoin . fromLineJoin . getLineJoin
         lDashing (getDashing -> Dashing ds offs) =
           C.setDash ds offs
+
+setSource :: Color c => c -> Style v -> C.Render ()
+setSource c s = C.setSourceRGBA r g b a'
+  where (r,g,b,a) = colorToRGBA c
+        a'        = case getOpacity <$> getAttr s of
+                      Nothing -> a
+                      Just d  -> a * d
+
 
 cairoTransf :: Transformation R2 -> C.Render ()
 cairoTransf t = C.transform m
@@ -173,7 +206,7 @@ fromLineJoin LineJoinRound = C.LineJoinRound
 fromLineJoin LineJoinBevel = C.LineJoinBevel
 
 instance Renderable Ellipse Cairo where
-  render _ ell = C $ do
+  render _ ell = C . lift $ do
     let P (xc,yc) = ellipseCenter ell
         (xs,ys)   = ellipseScale ell
         Rad th    = ellipseAngle ell
@@ -187,23 +220,34 @@ instance Renderable Ellipse Cairo where
     C.restore
 
 instance Renderable (Segment R2) Cairo where
-  render _ (Linear v) = C $ uncurry C.relLineTo v
-  render _ (Cubic (x1,y1) (x2,y2) (x3,y3)) = C $ C.relCurveTo x1 y1 x2 y2 x3 y3
+  render _ (Linear v) = C . lift $ uncurry C.relLineTo v
+  render _ (Cubic (x1,y1) (x2,y2) (x3,y3)) = C . lift $ C.relCurveTo x1 y1 x2 y2 x3 y3
 
 instance Renderable (Trail R2) Cairo where
   render _ (Trail segs c) = C $ do
     mapM_ renderC segs
-    when c C.closePath
+    lift $ when c C.closePath
 
 instance Renderable (Path R2) Cairo where
-  render _ (Path trs) = C $ C.newPath >> F.mapM_ renderTrail trs
+  render _ (Path trs) = C $ lift C.newPath >> F.mapM_ renderTrail trs
     where renderTrail (P p, tr) = do
-            uncurry C.moveTo p
+            lift $ uncurry C.moveTo p
             renderC tr
 
+-- see http://www.cairographics.org/tutorial/#L1understandingtext
 instance Renderable Text Cairo where
   render _ (Text tr str) = C $ do
-    C.save
-    cairoTransf (tr <> reflectionY)
-    C.showText str
-    C.restore
+    Alignment (xa,ya) <- gets getTextAlignment
+    lift $ do
+      C.save
+      cairoTransf (tr <> reflectionY)
+      tExt <- C.textExtents str
+      let w    = C.textExtentsWidth tExt
+          h    = C.textExtentsHeight tExt
+          refX = -w/2 - C.textExtentsXbearing tExt
+          refY = -h/2 - C.textExtentsYbearing tExt
+          newX = -xa*w/2
+          newY =  ya*h/2   -- I don't quite understand why this must be positive.
+      cairoTransf (moveOriginBy (newX - refX, newY - refY) mempty)  -- XXX
+      C.showText str
+      C.restore
