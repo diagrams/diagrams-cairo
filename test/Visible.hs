d9cdcff096af39e0426acd283ccf136d92723ef2
Brent Yorgey <byorgey@cis.upenn.edu>
2011-06-09 04:22:54 +0000

    import another test.  This one is rather slow and could be a nice example to help drive optimization.

---
+++ test/Visible.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+import Diagrams.Prelude
+import Diagrams.Backend.Cairo.CmdLine
+
+grid n = centerXY . cat unitY . map hcat $
+  (map . map) (lw 0 . applyColor) [ [(r,c) | c <- [0..n] ] |  r <- [0..n]]
+
+applyColor (0,0) = unitSquare # fc black
+applyColor (m,n) | gcd m n == 1 = unitSquare # fc yellow
+                 | otherwise    = unitSquare # fc black
+
+main = defaultMain (pad 1.1 $ grid 60)
