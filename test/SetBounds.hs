474ac02b3819c6ec8ae855956664bab9c4189f72
Brent Yorgey <byorgey@cis.upenn.edu>
2011-06-10 02:04:07 +0000

    import setBounds test

---
+++ test/SetBounds.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+import Diagrams.Prelude
+import Diagrams.Backend.Cairo.CmdLine
+
+main = defaultMain
+         (circle 2 |||
+          square 1.5 # rotateBy (1/8) # withBounds (square 1 :: Diagram Cairo R2))
