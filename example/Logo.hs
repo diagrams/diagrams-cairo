8fffacc5c2c1bcae492541a9ab73e189aade43c4
Brent Yorgey <byorgey@cis.upenn.edu>
2011-06-10 18:56:28 +0000

    fiddling around with logo

--- example/Logo.hs
+++ example/Logo.hs
@@ -3,6 +3,25 @@
 import Diagrams.Prelude
 import Diagrams.Backend.Cairo.CmdLine
 
+import qualified Data.Colour as C
+
+{-
+d = mconcat . map alignBR
+  $ [ dBody
+    , rect 0.5 6.5 # fc black
+    ]
+
+blues = iterate (C.blend 0.1 white) blue
+      # take 11 # reverse
+
+dBody = mconcat . reverse . zipWith fc blues . take 11 . iterate (rotateBy (-1/20)) . rotateBy (-1/4) $ halfC
+
+halfC = arc 0 (1/2 :: CircleFrac)
+      # scale 1.8
+      # stroke
+      # lw 0
+-}
+
 i = (circle 1 === strutY 0.5 === roundedRect (2,4) 0.4)
     # lw 0.05
     # lc blue
@@ -12,8 +31,9 @@ sierpinski 1 = polygon with { sides = 3, orientation = OrientToX }
 sierpinski n = s === (s ||| s)
   where s = sierpinski (n-1)
 
-a = sierpinski 3
+a = sierpinski 4
     # fc black
+    # scale (1/2)
 
 grid = verts # centerXY <> horiz # centerXY
   where verts = hcat' with {sep=0.5} $ replicate 20 (vrule 10)
@@ -30,7 +50,7 @@ m = square 5.5 <>
     text "m"
       # fontSize 6 # italic # font "freeserif" # fc green
 
-logo = (hcat' with {sep = 0.5} . map alignB $ [ i, a, m, g ])
+logo = (hcat' with {sep = 0.5} . map alignB $ [ i, a, g, m ])
        # centerXY
 
 main = defaultMain (pad 1.1 logo)
