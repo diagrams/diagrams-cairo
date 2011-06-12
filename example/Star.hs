20c8b240f998178ef66827e8dfd39a258636275d
Brent Yorgey <byorgey@cis.upenn.edu>
2011-06-12 19:27:10 +0000

    Star example (shows off named points)

---
+++ example/Star.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE NoMonomorphismRestriction, TupleSections #-}
+
+import Diagrams.Prelude
+import Diagrams.Backend.Cairo.CmdLine
+
+axes n = h <> v
+  where p = fromOffsets . replicate n
+        h = stroke' with {vertexNames = [map ("x",) [0..n]]} (p unitX)
+        v = stroke' with {vertexNames = [map ("y",) [0..n]]} (p unitY)
+
+connect n i = withAName ("x",i) $ \x ->
+              withAName ("y", n - i) $ \y ->
+                atop (stroke $ fromVertices [x, y])
+
+pic n = applyAll (map (connect n) [0..n]) (axes n) # centerXY # lw 0.05
+
+d n = half === rotateBy (1/2) half
+  where half = rotateBy (1/4) (pic n) ||| pic n
+
+main = defaultMain (pad 1.1 $ d 20 # lc blue)
