4245ad6d66752837a0c3ea3d0b3e49e6c3fc33ee
Brent Yorgey <byorgey@cis.upenn.edu>
2011-06-13 15:55:15 +0000

    very basic support for including external PNG images

--- src/Diagrams/Backend/Cairo.hs
+++ src/Diagrams/Backend/Cairo.hs
@@ -31,6 +31,7 @@ import Diagrams.Prelude
 import Diagrams.TwoD.Ellipse
 import Diagrams.TwoD.Path (Clip(..))
 import Diagrams.TwoD.Text
+import Diagrams.TwoD.Image
 import Diagrams.TwoD.Adjust (adjustDia2D)
 
 import qualified Graphics.Rendering.Cairo as C
@@ -39,6 +40,7 @@ import qualified Graphics.Rendering.Cairo.Matrix as CM
 import Control.Applicative ((<$>))
 import Control.Monad.State
 import Data.Maybe (catMaybes, fromMaybe)
+import Data.List (isSuffixOf)
 
 import Data.Monoid
 import qualified Data.Foldable as F
@@ -231,6 +233,19 @@ instance Renderable (Path R2) Cairo where
     where renderTrail (P p, tr) = do
             lift $ uncurry C.moveTo p
             renderC tr
+
+-- Can only do PNG files at the moment...
+instance Renderable Image Cairo where
+  render _ (Image file tr) = C . lift . when (".png" `isSuffixOf` file) $ do
+    C.save
+    cairoTransf (tr <> reflectionY)
+    pngSurf <- liftIO $ C.imageSurfaceCreateFromPNG file
+    w <- C.imageSurfaceGetWidth pngSurf
+    h <- C.imageSurfaceGetHeight pngSurf
+    C.setSourceSurface pngSurf (fromIntegral w/2)
+                               (-fromIntegral h/2)
+    C.paint
+    C.restore
 
 -- see http://www.cairographics.org/tutorial/#L1understandingtext
 instance Renderable Text Cairo where
---
+++ test/Image.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+import Diagrams.Prelude
+import Diagrams.Backend.Cairo.CmdLine
+
+daisyD = image "daisy-duck.png" 450 450
+
+daisy = mconcat . take 20 . iterate (rotateBy (1/15)) . alignB $ daisyD
+
+main = defaultMain (pad 1.1 daisy)
