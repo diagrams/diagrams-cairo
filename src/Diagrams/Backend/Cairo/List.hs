f89a4340a4abd4cb70bc79566f1975f524e504f8
nand <nand@lavabit.com>
2012-11-21 20:39:27 +0100

    Fix format decoding in 'renderToList' and output alpha

--- src/Diagrams/Backend/Cairo/List.hs
+++ src/Diagrams/Backend/Cairo/List.hs
@@ -3,7 +3,8 @@ module Diagrams.Backend.Cairo.List where
 import Control.Applicative ((<$>))
 import Control.Exception (bracket)
 
-import Data.Colour.SRGB (Colour, sRGB)
+import Data.Colour
+import Data.Colour.SRGB (sRGB)
 import Data.Word (Word8)
 
 import Diagrams.Prelude (Diagram, R2)
@@ -16,19 +17,19 @@ import Foreign.Marshal.Array (peekArray)
 -- | Render to a regular list of Colour values.
 
 renderToList :: (Ord a, Floating a) =>
-                  Int -> Int -> Diagram Cairo R2 -> IO [[Colour a]]
+                  Int -> Int -> Diagram Cairo R2 -> IO [[AlphaColour a]]
 renderToList w h d =
   f 0 <$> bracket (renderPtr w h d) free (peekArray $ w*h*4)
  where
-  f :: (Ord a, Floating a) => Int -> [Word8] -> [[Colour a]]
+  f :: (Ord a, Floating a) => Int -> [Word8] -> [[AlphaColour a]]
   f _ [] = []
   f n xs | n >= w = [] : f 0 xs
-  f n (_:r:g:b:xs) = let c = sRGB (l r) (l g) (l b) in
-    case f (n+1) xs of
+  f n (g:b:r:a:xs) =
+    let l n = fromIntegral n / fromIntegral a
+        c   = sRGB (l r) (l g) (l b) `withOpacity` (fromIntegral a / 255)
+
+    in case f (n+1) xs of
       []    -> [[c]]
       cs:ys -> (c:cs) : ys
 
   f _ _ = error "renderToList: Internal format error"
-
-  l :: (Ord a, Floating a) => Word8 -> a
-  l n = fromIntegral n / fromIntegral (maxBound :: Word8)
